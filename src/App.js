1b9fa694e064fd5a5f3acfb225704b4e50681e06
Brandon Tabaska <btabaska@gmail.com>
2020-10-19 15:01:48 -0700

    added hooks, async routing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1 id="something importent">Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1 id="something importent">Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 render(<App />, document.getElementById("root"));
